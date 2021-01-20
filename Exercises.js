3dd66d1c1b381aa4fa572a8519b5ff6e230e0803
yagelcohen <77248177+yagelcohen@users.noreply.github.com>
2021-01-20 20:47:27 +0200

    answer q2

--- Exercises.js
+++ Exercises.js
@@ -3,28 +3,35 @@ function myReverse(str) {
   return str.split("").reverse().join("");
     return "str";
 }
-  str = Number(myReverse(str))
+  str = Number(myReverse(str));
 
 //Question 2
-function allCombinations(str) {
-  let combinations = [];
-  // your code here
-
-  //
-  return combinations;
+let str =""
+let combinations = [];
+function allCombinations(str){
+    for (i=0; i<=str.length; i++){
+        for (j=i; j<=str.length; j++){
+         combinations.push(str.substring(i,j));
+        }
+    }
+    return combinations;
 }
-
+        allCombinations(str)
 //Question 3
+
 function allCaps(str) {
   // your code here
   return "";
 }
 
 //Question 4
+let answer = " "
 function myPower(x, n) {
-  // your code here
-  return "";
-}
+    answer = x**n 
+    return answer;
+  }
+  myPower(2,10)
+  console.log(answer);
 
 //Question 5
 function getFirstNotRepeating(str) {
